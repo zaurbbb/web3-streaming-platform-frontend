1b2c7a94f33da89d107ad671e6f3c329df7a57e3
Bioneisme <bioneisme@gmail.com>
2022-12-24 18:28:44 +0600

    Initial view all streams

---
+++ src/components/pages/ViewAllStreams/ViewAllStreamsPage.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import {useStream} from "@livepeer/react";
+import api from "../../../api";
+
+const ViewAllStreamsPage = () => {
+    const [streams, setStreams] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await api.get('/ipfs/getStreams');
+                setStreams(res.data);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    return (
+        <div>
+            {streams.map(stream => {
+                return (
+                    <div>
+                        <div>{stream.streamName}</div>
+                        <div>{stream.streamCategory}</div>
+                        <div>{stream.streamDescription}</div>
+                        <div>{stream.streamId}</div>
+                        <div>{stream.authorAddress}</div>
+                    </div>
+                )})}
+        </div>
+    );
+};
+
+export default ViewAllStreamsPage;
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 import CreateStreamPage from '../components/pages/CreateStreamPage/CreateStreamPage';
 import HomePage from '../components/pages/HomePage/HomePage';
 import ViewStreamPage from '../components/pages/ViewStreamPage/ViewStreamPage';
+import ViewAllStreamsPage from "../components/pages/ViewAllStreams/ViewAllStreamsPage";
 
 export const privateRoutes = [
 
@@ -10,4 +11,5 @@ export const publicRoutes = [
     { path: '/home', component: <HomePage />, id: 1 },
     { path: '/createStream', component: <CreateStreamPage/>, id: 2 },
     { path: '/viewStream/:streamID', component: <ViewStreamPage/>, id: 3 },
+    { path: '/viewAllStreams', component: <ViewAllStreamsPage/>, id: 4 },
 ];
